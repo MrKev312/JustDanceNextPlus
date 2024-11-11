468847bc01426bb20b4b55d97375dd285da6aa82
MrKev312 <34964788+MrKev312@users.noreply.github.com>
2024-11-11 20:30:21 +0100

    Add new classes and namespace for product management
    Introduced `ShopConfig` class with `FirstPartyProductDb` property.
    Added `FirstPartyProductDb` class with dictionaries for `DlcProducts`,
    `SubscriptionProducts`, and `ProductGroups`.
    Defined `DlcProduct` class with properties like `ClaimIds`, `FirstPartyId`,
    `Name`, `ProductLocId`, `Type`, `DlcType`, and `ProductDescriptionId`.
    Created `ProductGroup` class with properties including `Type`,
    `DisplayPriority`, `GroupLocId`, `Name`, `ProductIds`, `SongsCountLocId`,
    `GroupDescriptionLocId`, `TracklistExtended`, `TracklistLimited`,
    `TracklistExtendedLocId`, `TracklistLimitedLocId`, and `Assets`.
    Added `Assets` class with `ProductGroupBundle` property.

---
+++ JustDanceNextPlus/JustDanceClasses/Endpoints/ShopConfig.cs
@@ -0,0 +1,45 @@
+﻿namespace JustDanceNextPlus.JustDanceClasses.Endpoints;
+
+public class ShopConfig
+{
+	public FirstPartyProductDb FirstPartyProductDb { get; set; } = new();
+}
+
+public class FirstPartyProductDb
+{
+	public Dictionary<Guid, DlcProduct> DlcProducts { get; set; } = [];
+	public Dictionary<Guid, object> SubscriptionProducts { get; set; } = [];
+	public Dictionary<Guid, ProductGroup> ProductGroups { get; set; } = [];
+}
+
+public class DlcProduct
+{
+	public List<string> ClaimIds { get; set; } = [];
+	public string FirstPartyId { get; set; } = "";
+	public string Name { get; set; } = "";
+	public int ProductLocId { get; set; }
+	public string Type { get; set; } = "";
+	public string DlcType { get; set; } = "";
+	public int ProductDescriptionId { get; set; }
+}
+
+public class ProductGroup
+{
+	public string Type { get; set; } = "";
+	public int DisplayPriority { get; set; }
+	public int GroupLocId { get; set; }
+	public string Name { get; set; } = "";
+	public List<Guid> ProductIds { get; set; } = [];
+	public int SongsCountLocId { get; set; }
+	public int GroupDescriptionLocId { get; set; }
+	public List<Guid> TracklistExtended { get; set; } = [];
+	public List<Guid> TracklistLimited { get; set; } = [];
+	public int TracklistExtendedLocId { get; set; }
+	public int TracklistLimitedLocId { get; set; }
+	public Assets Assets { get; set; } = new();
+}
+
+public class Assets
+{
+	public string ProductGroupBundle { get; set; } = "";
+}
