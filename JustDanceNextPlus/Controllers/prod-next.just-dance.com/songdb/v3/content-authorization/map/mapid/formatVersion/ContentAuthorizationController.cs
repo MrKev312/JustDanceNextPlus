d57a83a784b0675ba77bb3792c731e1e1b97f1c8
MrKev312 <34964788+MrKev312@users.noreply.github.com>
2025-04-03 18:48:44 +0200

    Rename V0 to ContentAuthorizationController
    Updated constructor to include ILogger and MapService.
    Refactored HttpGet method to retrieve map codename by mapId,
    logging warnings for not found cases and returning appropriate
    responses based on the presence of ContentAuthorization in
    the SongDB.

--- JustDanceNextPlus/Controllers/prod-next.just-dance.com/songdb/v3/content-authorization/map/mapid/formatVersion/ContentAuthorizationController.cs
+++ JustDanceNextPlus/Controllers/prod-next.just-dance.com/songdb/v3/content-authorization/map/mapid/formatVersion/ContentAuthorizationController.cs
@@ -7,11 +7,24 @@ namespace JustDanceNextPlus.Controllers.prod_next.just_dance.com.songdb.v3.conte
 
 [ApiController]
 [Route("songdb/v3/content-authorization/map/{mapId:guid}/formatVersion/v0")]
-public class V0(MapService mapService) : ControllerBase
+public class ContentAuthorizationController(ILogger<ContentAuthorizationController> logger,
+	MapService mapService)
+	: ControllerBase
 {
 	[HttpGet]
 	public IActionResult Get([FromRoute] Guid mapId)
 	{
+		string mapCodename = mapService.MapToGuid
+			.FirstOrDefault(x => x.Value == mapId).Key;
+
+		if (string.IsNullOrEmpty(mapCodename))
+		{
+			logger.LogWarning("Map ID {MapId} not found in mapToGuid.", mapId);
+			return NotFound();
+		}
+
+		logger.LogInformation("Map {MapCodename} requested.", mapCodename);
+
 		return mapService.SongDB.ContentAuthorization.TryGetValue(mapId, out ContentAuthorization? contentAuthorization)
 			? Ok(contentAuthorization)
 			: NotFound();
